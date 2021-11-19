5e4388ac5d0bda4512dff9d44160ff3949d30709
Thomas BERTHET <thoberth@e2r12p21.clusters.42paris.fr>
2021-11-19 18:43:16 +0100

    1er pas en c++

---
+++ cpp_module_00/ex00/megaphone.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!" << std::endl;   // endl = fin de ligne = '\n' qu'on rajoute a la 
+    return 0;                                   // fin de Hello World!
+}                                               // cout = c-out = console out 
